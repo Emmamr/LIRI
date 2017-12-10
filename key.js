a02302ad43f0dd898f6eef3d71e108d0a99bdd61
Emmanuel Mosqueda Ramirez <emmanuelmosquedaramirez@Emmanuels.local>
2017-12-10 17:01:52 -0500

    initial site file

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('Keys loaded');
+
+var twitterKeys = {
+  consumer_key: '<5RhLRgoML0GQmv8NaSduVGRmC>',
+  consumer_secret: '<	lPOvJQvcHEqHAiIbCeE8gjfuLcp6znCU0HEO0x3vKBzfPulvFY>',
+  access_token_key: '<	939970986245525505-73fxNodGNFz6jsG1GdUMIsrDnUNHMxX>',
+  access_token_secret: '<	EPcAUJUScjL2SSojHoW4JiSKW5bQ1bVubdicTUMrlVlIT>',
+}
+
+module.exports = twitterKeys;
